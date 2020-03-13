8f6592d283c2e2d7563f726ef8a38e790e086ccd
Hasitha Kaushan <hkraclh@gmail.com>
2020-03-13 23:40:13 +0530

    initial commit

---
+++ src/main/java/com/example/cicdapp/CiCdAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdapp/CiCdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
